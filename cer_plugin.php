39bc68e411760f2c60ad30671780045bd6dbe1b2
kilozwo <kilozwo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-31 09:24:18 +0000

    first stable version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/comment-email-reply/tags/1.0@796513 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cer_plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Comment Email reply
+ * Plugin URI:  http://kilozwo.de/cer
+ * Description: Simply notifies comment-author via email if someone replies to his comment. Zero Configuration. Available in English and German.
+ * Version:     1.0
+ * Author:      Jan Eichhorn
+ * Author URI:  http://kilozwo.de
+ * License:     GPLv2
+ */
+
+load_plugin_textdomain('cer_plugin', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+add_action('wp_insert_comment','cer_comment_inserted',99,2);
+
+function cer_comment_inserted($comment_id, $comment_object) {
+    if ($comment_object->comment_parent > 0) {
+        $comment_parent = get_comment($comment_object->comment_parent);
+
+        $mailcontent = __('Hello ','cer_plugin').$comment_parent->comment_author.',<br>
+        '.$comment_object->comment_author.__(' replied to your comment on','cer_plugin').' <a href="'.get_permalink($comment_parent->comment_post_ID).'">'.get_the_title($comment_parent->comment_post_ID).'</a>:<br><br>
+        '.$comment_object->comment_content.'<br><br>'.
+        __('Go to it or reply:','cer_plugin').' <a href="'.get_comment_link( $comment_parent->comment_ID ).'">'.get_comment_link( $comment_parent->comment_ID ).'</a>';
+
+        wp_mail($comment_parent->comment_author_email,'['.get_option('blogname').']'.__('New reply to your Comment','cer_plugin'),$mailcontent);
+    }
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Comment Email Reply ===
+Contributors: kilozwo
+Donate link: http://kilozwo.de/donate
+Tags: Comments, Reply, Notification, Email,
+Requires at least: 3.0.1
+Tested up to: 3.7
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Simply notifies comment-author via email if someone replies to his comment. Zero Configuration.       
+
+== Description ==
+
+Simply notifies comment-author via email if someone replies to his comment. Zero Configuration.       
+
+
+== Installation ==
+
+1. Download the plugin-files from the WordPress Plugin Directory.
+2. Upload the files to your plugins directory on your Server.
+3. Activate the plugin in your admin-backend.
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 =
+* first stable version.
